4b24cce029f9db79c39083a33479cf443903c78f
GDRMVRM <gdrmvrm@gmail.com>
2022-05-04 16:05:05 +0100

    Signup page refactored in App.js

---
+++ client/src/components/SignUpPageDetails.jsx
@@ -0,0 +1,17 @@
+import NavBarLogin from './NavBarLogin';
+import SubBar from './SubBar';
+import FormSignUp from './FormSignUp';
+import SignInRedirection from './SignInRedirection';
+
+function SignUpPageDetails() {
+  return (
+    <>
+      <NavBarLogin />
+      <SubBar />
+      <FormSignUp />
+      <SignInRedirection />
+    </>
+  );
+}
+
+export default SignUpPageDetails;
