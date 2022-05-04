e672cc5933a7e6e4484c15a82c39f3e94d393c51
GDRMVRM <gdrmvrm@gmail.com>
2022-05-04 16:04:22 +0100

    MoodPage linked to the App.js

---
+++ client/src/components/MoodPageDetails.jsx
@@ -0,0 +1,13 @@
+import NavBarMoodPage from './NavBarMoodPage';
+import MoodPage from './MoodPage';
+
+function MoodPageDetails() {
+  return (
+    <>
+      <NavBarMoodPage />
+      <MoodPage />
+    </>
+  );
+}
+
+export default MoodPageDetails;
