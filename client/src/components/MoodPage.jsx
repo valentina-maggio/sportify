44689b1c5b6d8d64c5daf6579866eab3bc0e1c02
GDRMVRM <gdrmvrm@gmail.com>
2022-05-04 16:03:38 +0100

    Mood Page and style for images created

--- client/src/components/MoodPage.jsx
+++ client/src/components/MoodPage.jsx
@@ -1,13 +1,20 @@
 import { Link } from 'react-router-dom';
+import './MoodPage.css';
 
 function MoodPage() {
   return (
     <div className='mood-container'>
-      <h1>How do you feel today?</h1>
-      <div className='xxxxx'>
-        <Link to='/dashboard'>Profile</Link>
-        <Link to='/dasboard'>Calendar</Link>
-        <Link to='/dashboard'>Workouts</Link>
+      <h2>How do you feel today?</h2>
+      <div className='mood-links'>
+        <a href={Link}>
+          <img src='/images/low-impact.jpg' alt='low-impact' />
+        </a>
+        <a href={Link}>
+          <img src='/images/mid-impact.jpg' alt='mid-impact' />
+        </a>
+        <a href={Link}>
+          <img src='/images/high-impact.jpg' alt='high-impact' />
+        </a>
       </div>
     </div>
   );
