fdfd2fed827607537fb3e3f7e6eac5790efc42b8
Valentina <maggio.vale@gmail.com>
2022-05-05 12:19:08 +0100

    Started testing for date and time picker
    - need to find out out to mock the date in react testing library
    
    Co-authored-by: Gayathri Dharmavaram <gdrmvrm@users.noreply.github.com>

---
+++ client/src/__tests__/calendar.test.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Calendar from "../components/Calendar";
+
+test("shows the date and time picker", () => {
+
+// Need to find out how to mock the date in react testing library
+//   render(<Calendar />);
+
+//   const date = "05/05/2022 12:06 pm";
+
+//   const calendar = screen.getByLabelText(/select workout date & time/i);
+
+//   expect(calendar.value).toBe(date);
+});
