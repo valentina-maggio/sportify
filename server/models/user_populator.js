04b07cde5b9a55eb1d049f3a39bdd334905a757a
jazzc001 <jazzcheung212@gmail.com>
2022-05-05 15:35:46 +0100

    changed the userSchema to just have username, email and password.  All the corresponding areas are changed as well(both front end and backend)

--- server/models/user_populator.js
+++ server/models/user_populator.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 
-const MONGO_URL = `mongodb+srv://Jazzer:p%40ssw0rd%279%27%21@cluster0.mgq9v.mongodb.net/Sportify?retryWrites=true&w=majority`
+const MONGO_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.mgq9v.mongodb.net/Sportify?retryWrites=true&w=majority`
 
 
 mongoose.connect(MONGO_URL);
