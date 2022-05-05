f6d3e49e4082f493a2299f5ea90dd70a38b3555b
Valentina <maggio.vale@gmail.com>
2022-05-05 18:20:08 +0100

    Update WeatherAndForecast component to display the full week

--- client/src/components/WeatherAndForecast.jsx
+++ client/src/components/WeatherAndForecast.jsx
@@ -1,4 +1,3 @@
-
 import WeatherCurrent from "./WeatherCurrent";
 import WeatherForecast from "./WeatherForecast";
 
@@ -20,7 +19,7 @@ function WeatherAndForecast({ weatherInfo, location }) {
 
   const date = [];
 
-    for (let count = 0; count < 5; count+=1) {
+    for (let count = 0; count < 7; count+=1) {
       if (d.getDay() + count < 7) date[count] = d.getDay() + count;
       else if (d.getDay() + count === 7) date[count] = 0;
       else if (d.getDay() + count === 8) date[count] = 1;
@@ -33,19 +32,23 @@ function WeatherAndForecast({ weatherInfo, location }) {
       days[date[1]],
       days[date[2]],
       days[date[3]],
-      days[date[4]]
+      days[date[4]],
+      days[date[5]],
+      days[date[6]]
     ];
   }
 
   return (
-    <div className="WeatherAndForecast">
+    <div className="weatherandforecast">
       <WeatherCurrent weatherInfo={weatherInfo} location={location} date={date[0]} />
-      <div className="WeatherAndForecast__container">
+      <div className="weatherandforecast-container">
         <WeatherForecast weatherInfo={weatherInfo.daily[0]} date={date[0]} />
         <WeatherForecast weatherInfo={weatherInfo.daily[1]} date={date[1]} />
         <WeatherForecast weatherInfo={weatherInfo.daily[2]} date={date[2]} />
         <WeatherForecast weatherInfo={weatherInfo.daily[3]} date={date[3]} />
         <WeatherForecast weatherInfo={weatherInfo.daily[4]} date={date[4]} />
+        <WeatherForecast weatherInfo={weatherInfo.daily[5]} date={date[5]} />
+        <WeatherForecast weatherInfo={weatherInfo.daily[6]} date={date[6]} />
       </div>
     </div>
   );
