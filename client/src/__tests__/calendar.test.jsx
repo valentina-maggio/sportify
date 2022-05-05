2a2a29646d98c23d62043c45c140bd7aa7740bc1
Valentina <maggio.vale@gmail.com>
2022-05-05 12:21:00 +0100

    Started testing for date and time picker
    - need to find out out to mock the date in react testing library
    
    Co-authored-by: Gayathri Dharmavaram <gdrmvrm@users.noreply.github.com>

--- client/src/__tests__/calendar.test.jsx
+++ client/src/__tests__/calendar.test.jsx
@@ -9,7 +9,23 @@ test("shows the date and time picker", () => {
 
 //   const date = "05/05/2022 12:06 pm";
 
-//   const calendar = screen.getByLabelText(/select workout date & time/i);
+//  const calendar = screen.get(/select workout date & time/i);
 
 //   expect(calendar.value).toBe(date);
 });
+
+
+// Refer: Specify a now timestamp.
+
+// In app code:
+
+// // element
+// <div id="mockedDate"></div>
+
+// // script
+// document.getElementById('mockedDate').innerText = new Date();
+// it('stub date', () => {
+//     cy.clock(new Date(2020, 6, 24, 22, 19, 0).getTime());
+//     cy.visit('http://localhost:8081/?j=test');
+//     cy.get('#mockedDate').invoke('text').then(cy.log);
+// });
