ffb7aef07cddd2bce28b9a747f4b8e2ed73d4af2
Valentina <maggio.vale@gmail.com>
2022-05-07 10:59:37 +0100

    Start adding Videos components
    Co-authored-by: Gayathri Dharmavaram <gdrmvrm@users.noreply.github.com>

---
+++ client/src/components/VideoFiles.jsx
@@ -0,0 +1,74 @@
+import { Link } from 'react-router-dom';
+
+function VideoFiles() {
+  return (
+    <div>
+      <Link to="/">Sign in</Link>
+      <Link to="/">Sign in</Link>
+      <Link to="/">Sign in</Link>
+      <Link to="/">Sign in</Link>
+      <Link to="/">Sign in</Link>
+      <Link to="/">Sign in</Link>
+    </div>
+  )
+}
+
+export default VideoFiles;
+
+
+HIIT
+
+20 min
+https://www.youtube.com/watch?v=ByGi92AC95M&ab_channel=TheBodyCoachTV
+
+30 min
+https://www.youtube.com/watch?v=tXOZS3AKKOw&list=PLyCLoPd4VxBvRUzoyE1MDE_BR60d5B4JC&ab_channel=TheBodyCoachTV
+
+15 min
+legs burner
+https://www.youtube.com/watch?v=Txindp8nosc&ab_channel=TheBodyCoachTV
+
+10 min
+full body tabata
+https://www.youtube.com/watch?v=DDXBoIIecok&ab_channel=UPGRADEX
+
+25 min
+full body
+https://www.youtube.com/watch?v=ngu0XWgTiwU&t=33s&ab_channel=UPGRADEX
+
+
+STRENGHT
+
+20 min
+full body
+https://www.youtube.com/watch?v=mcVbdRS5_pY&ab_channel=TheBodyCoachTV
+
+11 min
+core strength
+https://www.youtube.com/watch?v=bhvU464BMbU&t=2s&ab_channel=UPGRADEX
+
+10 min
+lower body
+https://www.youtube.com/watch?v=_LdHzmgMTj0&ab_channel=UPGRADEX
+
+
+
+YOGA
+
+
+
+
+
+PILATES
+
+
+
+
+CARDIO
+ 
+
+
+LOW IMPACT
+
+20 min
+https://www.youtube.com/watch?v=gV-6ZySOq04&ab_channel=TheBodyCoachTV
