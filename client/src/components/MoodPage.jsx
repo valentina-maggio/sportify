f6ee4152d5f81e2178b70a94922086859f01356b
GDRMVRM <gdrmvrm@gmail.com>
2022-05-07 16:03:10 +0100

    videos of exercises included in the mood page

--- client/src/components/MoodPage.jsx
+++ client/src/components/MoodPage.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import VideoFiles from './VideoFiles';
 import './MoodPage.css';
 
@@ -7,15 +6,6 @@ function MoodPage() {
     <div className='mood-container'>
       <h2>How do you feel today?</h2>
       <div className='mood-links'>
-        <a href={Link}>
-          <img src='/images/low-impact.jpg' alt='low-impact' />
-        </a>
-        <a href={Link}>
-          <img src='/images/mid-impact.jpg' alt='mid-impact' />
-        </a>
-        <a href={Link}>
-          <img src='/images/high-impact.jpg' alt='high-impact' />
-        </a>
         <VideoFiles />
       </div>
     </div>
--- client/src/components/VideoFiles.jsx
+++ client/src/components/VideoFiles.jsx
@@ -1,15 +1,125 @@
+import React from 'react';
+import VideoFilesEmbed from './VideoFilesEmbed';
+import './VideoFiles.css';
+
 function VideoFiles() {
   return (
-    <div className="title">
-      <iframe title="HIIT Workout" width = "560" height="349" src="https://www.youtube.com/watch?v=ByGi92AC95M&ab_channel=TheBodyCoachTV" frameBorder="0" allowFullScreen />
-      <p> Full body HIIT </p>
-      <p> Duration: 20 minutes </p> 
+    <div className='video-container'>
+      <div className='video'>
+        <h3>Full Body HIIT</h3>
+        <h4>Duration: 20 minutes</h4>
+        <VideoFilesEmbed embedId='ByGi92AC95M' />
+      </div>
+      <div className='video'>
+        <h3>Full Body HIIT</h3>
+        <h4>Duration: 30 minutes</h4>
+        <VideoFilesEmbed embedId='tXOZS3AKKOw' />
+      </div>
+      <div className='video'>
+        <h3>Leg Burner HIIT</h3>
+        <h4>Duration: 15 minutes</h4>
+        <VideoFilesEmbed embedId='Txindp8nosc' />
+      </div>
+      <div className='video'>
+        <h3>Full Body Tabata</h3>
+        <h4>Duration: 10 minutes</h4>
+        <VideoFilesEmbed embedId='DDXBoIIecok' />
+      </div>
+      <div className='video'>
+        <h3>Kickboxing</h3>
+        <h4>Duration: 20 minutes</h4>
+        <VideoFilesEmbed embedId='OfZFdwW322E' />
+      </div>
+      <div className='video'>
+        <h3>Full Body Strength</h3>
+        <h4>Duration: 20 minutes</h4>
+        <VideoFilesEmbed embedId='mcVbdRS5_pY' />
+      </div>
+      <div className='video'>
+        <h3>Core Strength</h3>
+        <h4>Duration: 10 minutes</h4>
+        <VideoFilesEmbed embedId='bhvU464BMbU' />
+      </div>
+      <div className='video'>
+        <h3>Boots & Legs Strength</h3>
+        <h4>Duration: 25 minutes</h4>
+        <VideoFilesEmbed embedId='mrNQ7xHdlvo' />
+      </div>
+      <div className='video'>
+        <h3>Tones Arms Strength</h3>
+        <h4>Duration: 10 minutes</h4>
+        <VideoFilesEmbed embedId='l0CwCvJbGZI' />
+      </div>
+      <div className='video'>
+        <h3>Yoga for Beginners</h3>
+        <h4>Duration: 20 minutes</h4>
+        <VideoFilesEmbed embedId='vNyJuQuuMC8' />
+      </div>
+      <div className='video'>
+        <h3>Yoga for Stress Management</h3>
+        <h4>Duration: 30 minutes</h4>
+        <VideoFilesEmbed embedId='_zbtKeeAa-Y' />
+      </div>
+      <div className='video'>
+        <h3>Full Body Pilates</h3>
+        <h4>Duration: 20 minutes</h4>
+        <VideoFilesEmbed embedId='jHQnb7RfI58' />
+      </div>
+      <div className='video'>
+        <h3>Full Body Pilates</h3>
+        <h4>Duration: 20 minutes</h4>
+        <VideoFilesEmbed embedId='jHQnb7RfI58' />
+      </div>
+      <div className='video'>
+        <h3>Total Body Pilates</h3>
+        <h4>Duration: 30 minutes</h4>
+        <VideoFilesEmbed embedId='JLM3MKut7nE' />
+      </div>
+      <div className='video'>
+        <h3>Cardio Full Body</h3>
+        <h4>Duration: 15 minutes</h4>
+        <VideoFilesEmbed embedId='IvmaekQfKiw' />
+      </div>
+      <div className='video'>
+        <h3>Cardio Boxing</h3>
+        <h4>Duration: 30 minutes</h4>
+        <VideoFilesEmbed embedId='MqXMEhy43H0' />
+      </div>
+      <div className='video'>
+        <h3>Full Body Low Impact</h3>
+        <h4>Duration: 20 minutes</h4>
+        <VideoFilesEmbed embedId='gV-6ZySOq04' />
+      </div>
+      <div className='video'>
+        <h3>Full Body Low Impact</h3>
+        <h4>Duration: 15 minutes</h4>
+        <VideoFilesEmbed embedId='93bUXdhGI9k' />
+      </div>
     </div>
-  )
+  );
 }
 
 export default VideoFiles;
 
+// CARDIO
+
+// 15 min
+// full body
+// https://www.youtube.com/watch?v=IvmaekQfKiw&ab_channel=Juice%26Toya
+
+// 30 min
+// cardio boxing
+// https://www.youtube.com/watch?v=MqXMEhy43H0&ab_channel=fitness__kaykay
+
+// LOW IMPACT
+
+// 20 min
+// full body low impact
+// https://www.youtube.com/watch?v=gV-6ZySOq04&ab_channel=TheBodyCoachTV
+
+// 15 min
+// full body low impact
+// https://www.youtube.com/watch?v=93bUXdhGI9k&ab_channel=TheBodyCoachTV
 
 // HIIT
 
@@ -33,9 +143,6 @@ export default VideoFiles;
 // kickbox
 // https://www.youtube.com/watch?v=OfZFdwW322E&ab_channel=UtahLee
 
-
-
-
 // STRENGHT
 
 // 20 min
@@ -46,18 +153,13 @@ export default VideoFiles;
 // core strength
 // https://www.youtube.com/watch?v=bhvU464BMbU&t=2s&ab_channel=UPGRADEX
 
-// 10 min
-// lower body
-// https://www.youtube.com/watch?v=_LdHzmgMTj0&ab_channel=UPGRADEX
-
 // 25 min
 // Boot and legs
-// https://www.youtube.com/watch?v=mrNQ7xHdlvo&ab_channel=UPGRADEX 
+// https://www.youtube.com/watch?v=mrNQ7xHdlvo&ab_channel=UPGRADEX
 
 // 10 min
 // toned arms
 // https://www.youtube.com/watch?v=l0CwCvJbGZI&ab_channel=MadFit
-
 
 // YOGA
 
@@ -69,8 +171,6 @@ export default VideoFiles;
 // yoga for stress management
 // https://www.youtube.com/watch?v=_zbtKeeAa-Y&list=PLui6Eyny-UzwheLDyEScgdgbh7z3FgNCX&index=22&ab_channel=YogaWithAdriene
 
-
-
 // PILATES
 
 // 20 min
@@ -80,69 +180,3 @@ export default VideoFiles;
 // 30 min
 // total body
 // https://www.youtube.com/watch?v=JLM3MKut7nE&ab_channel=HeatherRobertson
-
-
-
-// CARDIO
-
-// 15 min
-// full body
-// https://www.youtube.com/watch?v=IvmaekQfKiw&ab_channel=Juice%26Toya
-
-// 30 min
-// cardio boxing
-// https://www.youtube.com/watch?v=MqXMEhy43H0&ab_channel=fitness__kaykay
-
-// LOW IMPACT
-
-// 20 min
-// full body low impact
-// https://www.youtube.com/watch?v=gV-6ZySOq04&ab_channel=TheBodyCoachTV
-
-// 15 min
-// full body low impact
-// https://www.youtube.com/watch?v=93bUXdhGI9k&ab_channel=TheBodyCoachTV 
-
-
-
-
-
-
-
-
-// import React, { Component } from "react";
-// import video1 from "./video/vd1.mp4";
-// import video2 from "./video/vd2.ogv";
-// import video3 from "./video/vd3.webm";
-
-// class Video extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <video src={video1} />
-//       </div>
-//     );
-//   }
-// }
-
-// export default Video;
-
-// import React, { Component } from "react";
-// import { PageHeader } from "react-bootstrap";
-// import Video from "./Video";
-
-// class Content extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <PageHeader className="title">
-//           <Video />  //Here just call your Video Component
-//           <small>Welcome to</small> <br />
-//           Profile Pulse
-//         </PageHeader>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Content;
