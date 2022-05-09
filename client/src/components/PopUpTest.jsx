9b924dfbedf6864dd2e71141ea97b49bd514413c
GDRMVRM <gdrmvrm@gmail.com>
2022-05-09 21:01:25 +0100

    exercise form clears after submit

--- client/src/components/PopUpTest.jsx
+++ client/src/components/PopUpTest.jsx
@@ -1,23 +1,22 @@
 import Popup from 'reactjs-popup';
-import axios from "axios";
-import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import axios from 'axios';
+import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 function PopUpTest() {
-
   const { state } = useLocation();
 
   console.log(`State passed is ${state}`);
 
-  const [exercise, setexercise] = useState({
-    name: "",
-    category: "",
-    intensity: "",
-    link: "",
+  const [exercise, setExercise] = useState({
+    name: '',
+    category: '',
+    intensity: '',
+    link: '',
   });
 
   const handleChange = (e) => {
-    setexercise({
+    setExercise({
       ...exercise,
       [e.target.name]: e.target.value,
     });
@@ -25,65 +24,64 @@ function PopUpTest() {
   const handleSubmit = async () => {
     try {
       console.log(exercise);
-      await axios.post("http://localhost:3001/exercises", exercise);
+      await axios.post('http://localhost:3001/exercises', exercise);
     } catch (error) {
-      console.log("Exercise could not be saved.", error.message);
+      console.log('Exercise could not be saved.', error.message);
     }
+    setExercise('');
   };
 
   return (
     <div>
       <h4>Create a New Workout</h4>
       {/* eslint-disable-next-line react/button-has-type */}
-      <Popup trigger={<button> Create </button>} 
-      position="right center">
+      <Popup trigger={<button> Create </button>} position='right center'>
         <div>
-        <h2>Exercise</h2>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleSubmit();
-          }}
-        >
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={exercise.name}
-            placeholder="Name"
-            onChange={handleChange}
-          />
-        <input
-          type="text"
-          id="category"
-          name="category"
-          value={exercise.category}
-          placeholder="Category"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          id="intensity"
-          value={exercise.intensity}
-          name="intensity"
-          placeholder="Intensity"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          id="link"
-          value={exercise.link}
-          name="link"
-          placeholder="Link"
-          onChange={handleChange}
-        />
-      </form>
-      <input type="submit" value="Save exercise" onClick={handleSubmit} />
+          <h2>Exercise</h2>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
+          >
+            <input
+              type='text'
+              id='name'
+              name='name'
+              value={exercise.name}
+              placeholder='Name'
+              onChange={handleChange}
+            />
+            <input
+              type='text'
+              id='category'
+              name='category'
+              value={exercise.category}
+              placeholder='Category'
+              onChange={handleChange}
+            />
+            <input
+              type='text'
+              id='intensity'
+              value={exercise.intensity}
+              name='intensity'
+              placeholder='Intensity'
+              onChange={handleChange}
+            />
+            <input
+              type='text'
+              id='link'
+              value={exercise.link}
+              name='link'
+              placeholder='Link'
+              onChange={handleChange}
+            />
+          </form>
+          <input type='submit' value='Save exercise' onClick={handleSubmit} />
+        </div>
+      </Popup>
     </div>
-      </Popup> 
-    </div>
-  )
+  );
 }
 
 export default PopUpTest;
-
