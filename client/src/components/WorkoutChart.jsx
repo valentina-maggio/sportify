69fdaaa83fda39e56d701754fd2caf3608aa2698
jazzc001 <jazzcheung212@gmail.com>
2022-05-10 12:20:57 +0100

    try to add the label function

--- client/src/components/WorkoutChart.jsx
+++ client/src/components/WorkoutChart.jsx
@@ -1,31 +1,54 @@
 import React from "react";
-import { PieChart, Pie, Tooltip } from "recharts";
+import { ResponsiveContainer, PieChart, Pie, Tooltip } from "recharts";
+
 
 const colors = ['#8884d8', '#9cacf1', '#8dd1e1', '#82ca9d', '#a4de6c', '#d0ed57']
 const data01 = [
-	{name: 'Yoga', value: 500, fill: colors[0],}, 
+  {name: 'Yoga', value: 500, fill: colors[0],}, 
 	{name: 'HIIT', value: 300, fill: colors[1],},
- 	{name: 'Cardio', value: 300, fill: colors[2]}, 
+  {name: 'Cardio', value: 300, fill: colors[2]}, 
   {name: 'Walking', value: 200, fill: colors[3]},
   {name: 'Weights', value: 278, fill: colors[4]}, 
   {name: 'Recovery', value: 189, fill: colors[5]},
 ];
 
+// const RADIAN = Math.PI / 180;
 
-
-
-
-
-function WorkoutChart() {
+// const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius }) => {
+  //   const radius = innerRadius + (outerRadius - innerRadius) * 1.2;
+  //   const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  //   const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  //   return (
+    
+    //     <text x={x} y={y} fill="black" textAnchor={x > cx ? 'start' : 'end'}  dominantBaseline="central">
+    //       {data01.map(i=> i.name)}
+    //     </text>
+    //   );
+    // };
+    
+    
+    function WorkoutChart() {
+      
   
 
-  return (
-    <PieChart width={800} height={400}>
-        <Pie isAnimationActive={false} data={data01} cx={240} cy={200} outerRadius={180} fill="#8884d8" label/>
-        
-        <Tooltip/>
-       </PieChart>
-  );
+      return (
+        <ResponsiveContainer>
+
+        <PieChart width={800} height={400}>
+            <Pie 
+            isAnimationActive={false} 
+            data={data01} 
+            cx={240} 
+            cy={200} 
+            outerRadius={180} 
+            fill="#8884d8" 
+            label={data01}/>
+            
+            
+            <Tooltip/>
+          </PieChart>
+        </ResponsiveContainer>
+      );
 }
 
 export default WorkoutChart;
