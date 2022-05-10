fca31605ca94d24387076cd7f9a174e38a2425d2
Valentina <maggio.vale@gmail.com>
2022-05-11 00:46:23 +0100

    Add styling for dashboard heading

---
+++ client/src/components/DashBoardHeading.jsx
@@ -0,0 +1,12 @@
+import './DashboardHeading.css';
+
+function DashboardHeading() {
+  return (
+    <div className='dashboard-heading'>
+      <h2>Not sure what workout to do?</h2>
+      <h2>We have some ideas here for you!</h2>
+    </div>
+  )
+}
+
+export default DashboardHeading;
