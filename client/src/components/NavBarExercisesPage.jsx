76b328dc961d3e2859dfd7f054f0ff5ac574f9a3
Valentina <maggio.vale@gmail.com>
2022-05-10 21:51:38 +0100

    Update navbars links

--- client/src/components/NavBarExercisesPage.jsx
+++ client/src/components/NavBarExercisesPage.jsx
@@ -8,7 +8,8 @@ function NavBarExercisesPage() {
         <img src='images/logo-and-name-copy.png' alt='Logo' />
       </div>
       <div>
-        <Link to='/workouts' className='workouts-link'>View Your Workouts</Link>
+        <Link to='/dashboard' className='dashboard-link'>Dashboard</Link>
+        <Link to='/workouts' className='workouts-link'>View Workouts</Link>
         <Link to='/' className='logout-link'>Logout</Link>
       </div>
     </div>
